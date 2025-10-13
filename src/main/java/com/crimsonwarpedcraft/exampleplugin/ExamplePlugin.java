ca0b6b16c7dad7e9fb94c06982e5b667e2a76dc6
kokodev <github@kokofixcomputers.serv00.net>
2025-10-13 12:08:31 -0700

    Update ExamplePlugin.java

--- src/main/java/com/crimsonwarpedcraft/exampleplugin/ExamplePlugin.java
+++ src/main/java/com/crimsonwarpedcraft/exampleplugin/ExamplePlugin.java
@@ -1,15 +1,9 @@
-package com.crimsonwarpedcraft.exampleplugin;
+package {{namespace}}.{{name}};
 
 import io.papermc.lib.PaperLib;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
-/**
- * Created by Levi Muniz on 7/29/20.
- *
- * @author Copyright (c) Levi Muniz. All Rights Reserved.
- */
-public class ExamplePlugin extends JavaPlugin {
+public class {{name}} extends JavaPlugin {
 
   @Override
   public void onEnable() {
